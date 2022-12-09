9eda9483d1b1cff9f3c2d67ab3a0a701f3d1145a
unknown <nuumri1115@gmail.com>
2022-12-09 14:30:26 +0800

    test

---
+++ index.js
@@ -0,0 +1,26 @@
+console.log("hi world");
+let message = "khuslen";
+console.log(message);
+const a = "ehhe";
+let b = "haha";
+var c = "hoho";
+console.log(a, b , c);
+let v =  'hello';
+v = 'hi';
+console.log(v);
+let z = "huslen";
+x = true;
+c = 20;
+n = null;
+m = undefined;
+console.log(typeof(z));
+console.log(typeof(x));
+console.log(typeof(c));
+console.log(typeof(n));
+console.log(typeof(m));
+let k = 23;
+i = 25;
+p = k + i;
+
+
+console.log(p);
